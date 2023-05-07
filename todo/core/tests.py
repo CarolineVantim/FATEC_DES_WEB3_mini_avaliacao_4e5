2b2973b2de4b419fe187a280049d625edaf543f9
CarolineVantim <carolvantim@gmail.com>
2023-05-07 01:50:13 -0300

    feature: implemented test and model change

--- todo/core/tests.py
+++ todo/core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from core.models import AtividadeModel
+from django.utils import timezone
 
-# Create your tests here.
+class AtividadeModelTest(TestCase):
+    def setUp(self):
+        self.atividade = AtividadeModel.objects.create(
+            nome='Atividade Teste',
+            descricao='Descrição da atividade de teste',
+            data=timezone.now())
+
+    def test_str(self):
+        self.assertEqual(str(self.atividade), 'Atividade Teste')
+
+    def test_verbose_names(self):
+        self.assertEqual(AtividadeModel._meta.verbose_name_plural, 'Atividades')
+        self.assertEqual(AtividadeModel._meta.verbose_name, 'Atividade')
